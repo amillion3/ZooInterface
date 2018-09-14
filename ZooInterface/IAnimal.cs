cc1545b588c2403333cfc93ee30a53740609fdb0
Andy Million <amillion3@gmail.com>
2018-09-14 12:11:41 -0500

    initial IAnimal setup

---
+++ ZooInterface/IAnimal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZooInterface
+{
+    interface IAnimal
+    {
+        bool NursesYoung { get; }
+        bool HairOnBody { get; }
+        bool Feathers { get; }
+        bool BornHardShellEgg { get; }
+        string LivesInWater { get; }
+        bool Gills { get; }
+        bool Scales { get; }
+        bool Fins { get; }
+        string Blooded { get; }
+        bool BornOnLand { get; }
+
+    }
+}
--- ZooInterface/Program.cs
+++ ZooInterface/Program.cs
@@ -2,11 +2,11 @@
 
 namespace ZooInterface
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
